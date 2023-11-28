e6162c4f58806977513650f2090facc0e1bf5f37
cozym <ddongyee99@daum.net>
2023-11-28 19:54:33 +0900

    feat: 사용자 입력 기능 구현

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,8 +1,28 @@
 package baseball;
 
-public class BaseballGame {
+import camp.nextstep.edu.missionutils.Console;
+import java.util.ArrayList;
+import java.util.List;
 
-    public void gameStart() {
-        
+public class BaseballGame {
+    List<Integer> user = new ArrayList<>();
+
+    public void inputNumber() {
+        try {
+            String input = Console.readLine();
+            for (int i = 0; i < input.length(); i++) {
+                int num = Integer.parseInt(String.valueOf(input.charAt(i)));
+                user.add(checkNum(num));
+            }
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int checkNum(int num) {
+        if (num < 1) {
+            throw new IllegalArgumentException();
+        }
+        return num;
     }
 }
